a3c63899145087dedbdc1c311c181b80e555c3cd
Trilochan Behera <127761645+trilochan-behera-dev@users.noreply.github.com>
2024-10-09 23:54:32 +0530

    Update Resume Link

--- src/components/Button/CVButton.js
+++ src/components/Button/CVButton.js
@@ -1,7 +1,7 @@
 export default function CVButton({ children, addClass }) {
 
     return (
-        <a href="https://drive.google.com/file/d/1eYA3WQ7jIVS3vKuziTfrBJLCRYon6ML_/view" target="_blank">
+        <a href="https://drive.google.com/file/d/1zlV2BYR0tF2OP1BhiBvaKVmBjMLCw6yg/view" target="_blank">
             <button
                 className={
                     "py-2 px-5 sm:px-8 text-white-500 font-semibold bg-primary hover:shadow-primary transition-all outline-none rounded-br-3xl rounded-md " +
